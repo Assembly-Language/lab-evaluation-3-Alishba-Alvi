58e11aae60100913bb7af396d2fd2cf7d2d9e6cb
Alishba-Alvi <alishbariasat123@gmail.com>
2024-12-30 11:28:29 -0800

    Question-commit

--- asm-file.asm
+++ asm-file.asm
@@ -11,8 +11,7 @@ asmfunc PROC p1:DWORD, p2:DWORD
     
     push ebp
     mov ebp,esp
-    mov eax, [ebp+12
-\] ; Move the first parameter into EAX
+    mov eax, [ebp+12] ; Move the first parameter into EAX
     call writedec
     call crlf
     ;add eax, p2 ; Add the second parameter to EAX
